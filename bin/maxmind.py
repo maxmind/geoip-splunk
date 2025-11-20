7465059a9f17eda69e0dd58ca6f06ad655e577ef
William Storey <wstorey@maxmind.com>
2025-11-20 14:58:42 -0800

    WIP

---
+++ bin/maxmind.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# from https://github.com/splunk/splunk-app-examples/blob/master/custom_search_commands/python/customsearchcommands_template/bin/stream.py
+
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))
+
+from splunklib.searchcommands import \
+    dispatch, StreamingCommand, Configuration, Option, validators
+
+
+@Configuration()
+class %(command.title())Command(StreamingCommand):
+    """ %(synopsis)
+
+    ##Syntax
+
+    %(syntax)
+
+    ##Description
+
+    %(description)
+
+    """
+    def stream(self, events):
+
+        # To connect with Splunk, use the instantiated service object which is created using the server-uri and
+        # other meta details and can be accessed as shown below
+        # Example:-
+        #    service = self.service
+
+        # Put your event transformation code here
+        for event in events:
+            yield event
+
+dispatch(%(command.title())Command, sys.argv, sys.stdin, sys.stdout, __name__)
