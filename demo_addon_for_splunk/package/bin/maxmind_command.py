a5f6f6f1b4420780548a946b3daacd46e645d89c
William Storey <wstorey@maxmind.com>
2025-11-24 11:43:26 -0800

    Clean up command code

--- demo_addon_for_splunk/package/bin/maxmind_command.py
+++ demo_addon_for_splunk/package/bin/maxmind_command.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import sys
 import os
 
@@ -7,7 +5,14 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))
 
 import maxminddb
 
-# Open the MaxMind database once at module level
+# Open the database once at module level
+#
+# TODO: We need to be able to re-open this when there are updates.
+#
+# TODO: We need to be able to handle multiple databases.
+#
+# TODO: We need to be able to download databases rather than assume they are
+# available in the add-on directly.
 script_dir = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(script_dir, '..', 'data', 'GeoLite2-Country.mmdb')
 _reader = maxminddb.open_database(db_path)
@@ -23,17 +28,13 @@ def stream(command, events):
     Yields:
         Modified event dictionaries with country_code field added
     """
-    # Get the IP field name from the command options
     ip_field = command.ip_field
 
-    # Process each event
     for event in events:
-        # Get the IP address from the specified field
         ip_address = event.get(ip_field)
 
         if ip_address:
             try:
-                # Perform the MaxMind database lookup
                 record = _reader.get(ip_address)
 
                 # Add the country code to the event if found
@@ -41,7 +42,6 @@ def stream(command, events):
                     event['country_code'] = record['country']['iso_code']
 
             except ValueError:
-                # Invalid IP address - skip adding fields
                 pass
 
         yield event
