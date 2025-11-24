b0e21f20b3ff3baf2d4fdddaec434b28118f444f
William Storey <wstorey@maxmind.com>
2025-11-24 09:59:04 -0800

    Remove files before building

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 set -eu -o pipefail
 
+rm -rf output *.tar.gz
 uv run -- ucc-gen build --source demo_addon_for_splunk/package --ta-version 1.0.0
 uv run -- ucc-gen package --path output/demo_addon_for_splunk
