53abf5599fa79d1175d780285312d9322c6e390d
William Storey <wstorey@maxmind.com>
2026-04-01 15:00:44 -0700

    Bump to v1.1.0

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 
 rm -rf output *.tar.gz
 find geoip/package -type d -name "__pycache__" -prune -exec rm -rf {} +
-uv run -- ucc-gen build --source geoip/package --ta-version 1.0.0
+uv run -- ucc-gen build --source geoip/package --ta-version 1.1.0
 
 # Clean up pip-installed files that AppInspect doesn't like
 # - .hash directories from aiohttp (Cython build artifacts)
